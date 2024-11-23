c1bc003b4298b000f95f501e803b2fff52558016
Bruno-Depetris <Depetrisbruno@gmail.com>
2024-11-23 01:16:58 +0100

    opcion uno

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ function cycleImages() {
 }
 
 setInterval(cycleImages, 1500); 
+
+
+const productos = [
+    { img: './data/MANDIRA/Page/Publicaciones/2.png', title: 'Cartera marrón', alt: 'Cartera marrón' ,precio:'200' },
+    { img: './data/MANDIRA/Page/Publicaciones/3.png', title: 'Mochila negra', alt: 'Mochila negra' ,precio:'200'},
+    { img: './data/MANDIRA/Page/Publicaciones/4.png', title: 'Riñonera azul', alt: 'Riñonera azul',precio:'200' },
+    // Agrega más productos según sea necesario
+];
+
+const publicacionesDiv = document.getElementById('publicaciones');
+
+// Generar dinámicamente las publicaciones
+productos.forEach(producto => {
+    const article = document.createElement('article');
+    article.innerHTML = `
+        <img src="${producto.img}" alt="${producto.alt}">
+        <h4>${producto.title}</h4>
+        <p>$  ${producto.precio}</p>
+        <button>Comprar</button>
+    `;
+    publicacionesDiv.appendChild(article);
+});
